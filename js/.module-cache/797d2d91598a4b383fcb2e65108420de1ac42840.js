8709e39a76e1225c5b48922ccab22401ddfd3a50
Tom <fredwonneberger@gmail.com>
2015-06-03 18:30:39 +0000

    cleaned up module files

--- js/.module-cache/797d2d91598a4b383fcb2e65108420de1ac42840.js
+++
@@ -1,8 +0,0 @@
-// React.render(
-//   <h1>Hello, world!</h1>,
-//   document.getElementById('bulletin')
-// );
-
-var CommentBox = React.createClass({displayName: "CommentBox",
-    rend
-})
--- js/.module-cache/9cbf1873fdd436f0ee6249ddc81b598e7e8616ab.js
+++
@@ -1,6 +0,0 @@
-// React.render(
-//   <h1>Hello, world!</h1>,
-//   document.getElementById('bulletin')
-// );
-
-var Commentbox
